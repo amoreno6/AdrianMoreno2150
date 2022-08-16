e21559484822232ae50fc803cd08b626366057a8
amoreno6 <111070003+amoreno6@users.noreply.github.com>
2022-08-16 11:34:53 -0400

    Create IncorrectSwap.java

---
+++ IncorrectSwap.java
@@ -0,0 +1,14 @@
+public class PrimitiveParameters {
+    public static void main(String[] args) {
+        int a = 10, b = 5;
+        System.out.printf("Before incorrectSwap: a=%d, b=%d\n", a, b);
+        incorrectSwap(a, b);
+        System.out.printf("After incorrectSwap: a=%d, b=%d\n", a, b);
+    }
+    
+    public static void incorrectSwap(int a, int b) {
+        int tmp = a;
+        a = b;
+        b = tmp;
+    }
+}
