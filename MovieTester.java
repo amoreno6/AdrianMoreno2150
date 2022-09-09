9877a8159bc629bb5c656292a4b0ea184c050f09
amoreno6 <111070003+amoreno6@users.noreply.github.com>
2022-09-08 20:14:19 -0400

    Create MovieTester.java

---
+++ MovieTester.java
@@ -0,0 +1,15 @@
+/**
+ * ITEC 2150
+ * Created By Adrian Moreno
+ * 9/8/2022
+ * Purpose is to test Movie.java getters and setters
+ */
+public class MovieTester
+{
+	public static void main (String [] args)
+	{
+		Movie m1 = new Movie("The Wolf of Wall Street", 2013, "Martin Scorcese", "Leonardo Dicaprio");
+		
+		System.out.println(m1);
+	}
+}
