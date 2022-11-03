4d96a73c28bbe646b636d51afa07d3a65eeb2d9a
amoreno6 <111070003+amoreno6@users.noreply.github.com>
2022-11-03 12:41:07 -0400

    Create SplitStack.java

---
+++ SplitStack.java
@@ -0,0 +1,7 @@
+/** Class: SplitStack.java
+*@author Adrian Moreno
+* Course: ITEC 2150
+* Written: 11/3/2022
+*
+* This class - Takes a stack of integers as a parameter and splits it into negatives and non-negatives
+*//
